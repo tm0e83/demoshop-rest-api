b38509e4796d3360cc3d541512b543df42b291a6
tm0e83 <tm0e83@gmail.com>
2026-07-14 14:45:12 +0200

    get products route

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import { db } from '../firebase.js';
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.json({ products: [] });
+router.get('/', async (req: Request, res: Response) => {
+  const snapshot = await db.collection('products').get();
+  const products = snapshot.docs.map((doc) => doc.data());
+  res.json(products);
 });
 
 export { router as productsRouter };
